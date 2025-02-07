320edb13308aada12fc8cb98920b2afc0e057796
utkarshsingh004 <singhutkarshkumar097@gmail.com>
2025-02-07 05:43:15 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,137 +1,118 @@
-//App.js
+import { useEffect, useState } from 'react';
 
-import { Oval } from 'react-loader-spinner';
-import React, { useState } from 'react';
-import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFrown } from '@fortawesome/free-solid-svg-icons';
-import './App.css';
+function App() {
+  const date = new Date();
+  const months = [
+    "January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"
+  ];
+  
+  const month = months[date.getMonth()];
+  const dates = date.getDate();
+  const year = date.getFullYear();
+  const formatedDate = `${month} ${dates}, ${year}`;
 
-function WeatherApp() {
-	const [input, setInput] = useState('');
-	const [weather, setWeather] = useState({
-		loading: false,
-		data: {},
-		error: false,
-	});
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(false);
 
-	const toDateFunction = () => {
-		const months = [
-			'January',
-			'February',
-			'March',
-			'April',
-			'May',
-			'June',
-			'July',
-			'August',
-			'September',
-			'October',
-			'November',
-			'December',
-		];
-		const WeekDays = [
-			'Sunday',
-			'Monday',
-			'Tuesday',
-			'Wednesday',
-			'Thursday',
-			'Friday',
-			'Saturday',
-		];
-		const currentDate = new Date();
-		const date = `${WeekDays[currentDate.getDay()]} ${currentDate.getDate()} ${months[currentDate.getMonth()]
-			}`;
-		return date;
-	};
+  const API_KEY = 'b24e968e351fe46fdb723ee595f53d9f';
 
-	const search = async (event) => {
-		if (event.key === 'Enter') {
-			event.preventDefault();
-			setInput('');
-			setWeather({ ...weather, loading: true });
-			const url = 'https://api.openweathermap.org/data/2.5/weather';
-			const api_key = 'f00c38e0279b7bc85480c3fe775d518c';
-			await axios
-				.get(url, {
-					params: {
-						q: input,
-						units: 'metric',
-						appid: api_key,
-					},
-				})
-				.then((res) => {
-					console.log('res', res);
-					setWeather({ data: res.data, loading: false, error: false });
-				})
-				.catch((error) => {
-					setWeather({ ...weather, data: {}, error: true });
-					setInput('');
-					console.log('error', error);
-				});
-		}
-	};
+  const fetchWeather = async (lat, lon) => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+      if (lat && lon) {
+        url += `&lat=${lat}&lon=${lon}`;
+      } else {
+        url += `&q=${city}`;
+      }
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.cod === "404") {
+        setError(true);
+        setWeatherData(null);
+      } else {
+        setError(false);
+        setWeatherData(data);
+      }
+    } catch (error) {
+      console.log("Some Unexpected Error::", `${error}`);
+    }
+  };
 
-	return (
-		<div className="App">
-			<h1 className="app-name">
-				 Weather App
-			</h1>
-			<div className="search-bar">
-				<input
-					type="text"
-					className="city-search"
-					placeholder="Enter City Name.."
-					name="query"
-					value={input}
-					onChange={(event) => setInput(event.target.value)}
-					onKeyPress={search}
-				/>
-			</div>
-			{weather.loading && (
-				<>
-					<br />
-					<br />
-					<Oval type="Oval" color="black" height={100} width={100} />
-				</>
-			)}
-			{weather.error && (
-				<>
-					<br />
-					<br />
-					<span className="error-message">
-						<FontAwesomeIcon icon={faFrown} />
-						<span style={{ fontSize: '20px' }}>City not found</span>
-					</span>
-				</>
-			)}
-			{weather && weather.data && weather.data.main && (
-				<div>
-					<div className="city-name">
-						<h2>
-							{weather.data.name}, <span>{weather.data.sys.country}</span>
-						</h2>
-					</div>
-					<div className="date">
-						<span>{toDateFunction()}</span>
-					</div>
-					<div className="icon-temp">
-						<img
-							className=""
-							src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
-							alt={weather.data.weather[0].description}
-						/>
-						{Math.round(weather.data.main.temp)}
-						<sup className="deg">°C</sup>
-					</div>
-					<div className="des-wind">
-						<p>{weather.data.weather[0].description.toUpperCase()}</p>
-						<p>Wind Speed: {weather.data.wind.speed}m/s</p>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        fetchWeather(position.coords.latitude, position.coords.longitude);
+      }, () => {
+        fetchWeather();
+      });
+    } else {
+      fetchWeather();
+    }
+  }, []);
+
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchWeather();
+    setCity('');
+  };
+
+  const getWeatherIconUrl = (main) => {
+    switch (main) {
+      case "Clouds":
+        return "/sun.png";
+      case "Rain":
+        return "/rain_with_cloud.png";
+      case "Mist":
+        return "/Tornado.png";
+      case "Haze":
+        return "/sun.png";
+      case "Clear":
+        return "/clear.png";
+      case "Thunderstorm":
+        return "/thunder.png";
+      case "Fog":
+        return "/fog.png";
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
+      <div className="App">
+        <div className="container">
+          {weatherData && (
+            <>
+              <h1 className="container_date">{formatedDate}</h1>
+              <div className="weather_data">
+                <h2 className="container_city">{weatherData.name}, {weatherData.sys.country}</h2>
+                <img src={getWeatherIconUrl(weatherData.weather[0].main)} alt="weather_image" width="180px" />
+                <h2 className="container_degree">{(weatherData.main.temp - 273.15).toFixed(2)}°C</h2>
+                <h2 className="container_para">{weatherData.weather[0].main}</h2>
+                <form className='form' onSubmit={handleSubmit}>
+                  <input type="text" className='input' placeholder='Enter City Here....' onChange={handleInputChange} value={city} />
+                  <br />
+                  <button type='submit'> Search </button>
+                </form>
+              </div>
+            </>
+          )}
+
+          {error && (
+            <>
+               <h1 className="errors"> {"city not found"}{"😔"}</h1>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default WeatherApp;
+export default App;
